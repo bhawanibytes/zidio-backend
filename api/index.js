6088c8678447d566fcc784603d81fd73ad9f1235
Bhawani Singh <bs190520@gmail.com>
2024-10-09 22:54:09 +0530

    adjust structure for vercel and created db models

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
+const mongoose = require("mongoose");
 
 const corsOptions = {
     origin: 'https://zidio-web.vercel.app',
@@ -16,11 +17,30 @@ app.get("/", (req, res) =>{
     });
 });
 
+app.post("/contact", (req, res) => {
+    res.status(200).json({
+        message: "We will contact you, Shorty!"
+    })
+})
+
 app.post("/subcriber",(req, res) =>{
     res.status(200).json({
-        message: "this ep needs to finish"
+        message: "You subcribed to newsletter, successfully!"
     });
 });
 
-app.listen(`${process.env.port}`)
+async function main() {
+    try {
+        await mongoose.connect(`${process.env.mongoDbString}`);
+        console.log("db is connected")
+        app.listen(`${process.env.PORT}`, () => {
+            console.log(`Listening on PORT: ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Database connection error:", error);
+    }
+}
+
+main();
+
 module.exports = app;
